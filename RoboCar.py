d71e8e8e0975657090283e13b7d2e2eb36e5dd06
Lasse-hash <Lasse.ulrik.sondergaard@gmail.com>
2025-12-01 13:42:16 +0100

    Create RoboCar.py

---
+++ RoboCar.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+
+
+class RoboCar:  def __init__(self, model, year):
+        self.model = model
+        self.year = year                            
+
+
+    def start_engine(self):
+        print(f"The engine of the {self.year} {self.model} is starting.")   
+
+
+    def stop_engine(self):              
+        print(f"The engine of the {self.year} {self.model} is stopping.")
+
+
+    def drive(self, speed):
+        print(f"The {self.year} {self.model} is driving at {speed} mph.")       
