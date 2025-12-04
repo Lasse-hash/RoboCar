35ba0a0fed67476b0662719e78737711a30609d5
Mads-Laursen <58849@student.eadania.dk>
2025-12-04 10:49:14 +0100

    Update Sumo.py

--- Sumo.py
+++ Sumo.py
@@ -1,14 +1,132 @@
 import RPi.GPIO as GPIO
 import time
+from sshkeyboard import listen_keyboard
+import random
+import threading
 
 # Use BCM (Broadcom pin numbering)
 GPIO.setmode(GPIO.BCM)
+# --- Left motor ---
+pwm1_left = 13
+pwm2_left = 12
+dir_left_fwd = 17
+dir_left_bwd = 23
 
-TRIG = 23   # Example GPIO pin for Trigger
-ECHO = 24   # Example GPIO pin for Echo
+# --- Right motor ---
+pwm1_right = 19
+pwm2_right = 18
+dir_right_fwd = 27
+dir_right_bwd = 22
+
+GPIO_PINH = 24
+GPIO_PINV = 26
+
+TRIG = 3   # Example GPIO pin for Trigger
+ECHO = 2   # Example GPIO pin for Echo
+
+sensor_delay = 0.003
+
+for pin in [pwm1_left, pwm2_left, dir_left_fwd, dir_left_bwd,
+            pwm1_right, pwm2_right, dir_right_fwd, dir_right_bwd]:
+    GPIO.setup(pin, GPIO.OUT)
+
+# --- PWM objects ---
+pwmL1 = GPIO.PWM(pwm1_left, 1000)
+pwmL2 = GPIO.PWM(pwm2_left, 1000)
+pwmR1 = GPIO.PWM(pwm1_right, 1000)
+pwmR2 = GPIO.PWM(pwm2_right, 1000)
+
+pwmL1.start(0)
+pwmL2.start(0)
+pwmR1.start(0)
+pwmR2.start(0)
 
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
+
+running = True
+
+delayTime = 0.1
+
+def press(key):
+    global running
+    if key == "q":
+        print("stopping")
+        running = False
+        stop()        
+
+
+def stop():
+    
+
+    GPIO.output(dir_left_fwd, False)
+    GPIO.output(dir_left_bwd, False)
+    GPIO.output(dir_right_fwd, False)
+    GPIO.output(dir_right_bwd, False)
+
+    pwmL1.ChangeDutyCycle(0)
+    pwmL2.ChangeDutyCycle(0)
+    pwmR1.ChangeDutyCycle(0)
+    pwmR2.ChangeDutyCycle(0)
+
+    print("STOPPED")
+
+    GPIO.cleanup()
+
+
+def forward():
+    GPIO.output(dir_left_fwd, True)
+    GPIO.output(dir_left_bwd, False)
+
+    GPIO.output(dir_right_fwd, False)
+    GPIO.output(dir_right_bwd, True)
+
+    pwmL1.ChangeDutyCycle(30)
+    pwmL2.ChangeDutyCycle(30)
+    pwmR1.ChangeDutyCycle(30)
+    pwmR2.ChangeDutyCycle(30)
+
+
+def turn_left():
+    print("Turning LEFT...")
+    GPIO.output(dir_left_bwd, False)
+    GPIO.output(dir_left_fwd, True)
+
+    GPIO.output(dir_right_fwd, True)
+    GPIO.output(dir_right_bwd, False)
+
+    pwmL1.ChangeDutyCycle(50)
+    pwmL2.ChangeDutyCycle(65)
+    pwmR1.ChangeDutyCycle(65)
+    pwmR2.ChangeDutyCycle(75)
+
+
+def turn_right():
+    print("Turning RIGHT...")
+    GPIO.output(dir_left_bwd, True)
+    GPIO.output(dir_left_fwd, False)
+
+    GPIO.output(dir_right_fwd, False)
+    GPIO.output(dir_right_bwd, True)
+
+    pwmL1.ChangeDutyCycle(65)
+    pwmL2.ChangeDutyCycle(75)
+    pwmR1.ChangeDutyCycle(50)
+    pwmR2.ChangeDutyCycle(65)
+
+def slam():
+    print("Slamming")
+    
+    GPIO.output(dir_left_fwd, True)
+    GPIO.output(dir_left_bwd, False)
+
+    GPIO.output(dir_right_fwd, False)
+    GPIO.output(dir_right_bwd, True)
+
+    pwmL1.ChangeDutyCycle(90)
+    pwmL2.ChangeDutyCycle(90)
+    pwmR1.ChangeDutyCycle(90)
+    pwmR2.ChangeDutyCycle(90)
 
 def get_distance():
     # Ensure trigger is low
@@ -33,15 +151,46 @@ def get_distance():
     distance_cm = pulse_duration * 34300 / 2
     return distance_cm
 
-try:
-    while True:
-        dist = get_distance()
-        if dist > 2 and dist < 450:  # working range of RCWL‑1601 per datasheet
-            print(f"Distance: {dist:.1f} cm")
-        else:
-            print("Out of range")
-        time.sleep(1)
+def Sumo():
+    global running
+    print("STARTING!")  
+    while running:
 
-except KeyboardInterrupt:
-    print("Measurement stopped by user")
-    GPIO.cleanup()
+        dist = get_distance()
+        
+        right = GPIO.input(GPIO_PINV)
+        left = GPIO.input(GPIO_PINH)
+
+        time.sleep(sensor_delay)
+
+        right2 = GPIO.input(GPIO_PINV)
+        left2 = GPIO.input(GPIO_PINH)
+        
+        if left != left2 or right != right2:
+            continue  
+
+        if GPIO.input(GPIO_PINV) == GPIO.LOW and GPIO.input(GPIO_PINH) == GPIO.LOW:
+            forward()
+
+        elif GPIO.input(GPIO_PINH) == GPIO.HIGH:
+            turn_right()
+            time.sleep(random.uniform(0.5, 1))
+
+        elif GPIO.input(GPIO_PINV) == GPIO.HIGH:
+            turn_left()
+            time.sleep(random.uniform(0.5, 1))
+
+        if dist >= 20:
+            slam()
+        
+
+        time.sleep(delayTime)
+
+threading.Thread(
+    target=lambda: listen_keyboard(on_press=press),
+    daemon=True
+).start()
+
+Sumo()
+
+
